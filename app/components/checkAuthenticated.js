1545ec7e49bc73227f99787d7b33f1e8dd6af15b
Sam Wang <me@here.com>
2021-03-11 13:59:46 -0500

    cleanup unused import

--- app/components/checkAuthenticated.js
+++ app/components/checkAuthenticated.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { isAuthenticated, selectUser, selectNetworkSrror, selectLoading } from '../slices/userSlice';
+import { isAuthenticated } from '../slices/userSlice';
 
 export default CheckAuthenticated = () => {
     const dispatch = useDispatch();
